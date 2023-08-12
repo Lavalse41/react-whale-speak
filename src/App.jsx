bb7b2338a6e06bb40652c0ee70a04f06906cedc1
unknown <ratiporn.pr@gmail.com>
2023-08-12 11:53:51 +0700

    style: overall

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="pt-9 bg-sky-100 w-screen h-screen flex flex-col items-center">
+      <Logo />
+      <Translator />
+      <History />
+    </div>
+  );
 }
 
-export default App
+function Logo() {
+  return (
+    <div className="text-center">
+      <h1 className="text-5xl font-caprasimo">Whale Speak</h1>
+      <p className="text-2xl">Translate English to Whale language</p>
+    </div>
+  );
+}
+
+function Translator() {
+  const [text, setText] = useState("");
+  return (
+    <div className="mt-9 w-[800px] py-6 rounded-xl bg-sky-700 text-center">
+      <div className="text-2xl text-slate-50 ">text</div>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        className="p-2 mt-4 w-[400px] rounded-xl"
+      ></input>
+      <br></br>
+      <button className="mt-4 p-1 w-[200px] rounded-full bg-pink-300">
+        Speak
+      </button>
+    </div>
+  );
+}
+
+function History() {
+  return (
+    <div className="mt-7 py-6 px-6 w-[800px] py-6 rounded-xl bg-sky-200">
+      <h2 className="text-lg">History</h2>
+      <ul>
+        <li className="flex flex-row justify-between text-sky-900">
+          <span>text 1</span>
+          <span>translate 1</span>
+          <button className="text-pink-600">❌</button>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
